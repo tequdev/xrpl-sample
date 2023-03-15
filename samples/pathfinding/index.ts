27fcc29df5c5f586ccabb45bb45a007554a21efe
tequ <develoQ.jp@gmail.com>
2023-03-15 22:14:29 +0900

    fix pathfinding

--- samples/pathfinding/index.ts
+++ samples/pathfinding/index.ts
@@ -1,4 +1,4 @@
-import { Client, PathFindCreateRequest, xrpToDrops } from 'xrpl'
+import { Client, xrpToDrops } from 'xrpl'
 
 const client = new Client('wss://xrpl.ws')
 
@@ -6,17 +6,15 @@ const main = async () => {
   await client.connect()
   
   client.on('path_find', (stream) => {
-    console.log(JSON.stringify(stream, null, '  '))
+    console.log(JSON.stringify(stream.alternatives, null, '  '))
   })
 
-  client.request<PathFindCreateRequest,any>({
+  client.request({
     command: 'path_find',
     subcommand: 'create',
     source_account: 'rQQQrUdN1cLdNmxH4dHfKgmX5P4kf3ZrM',
     destination_account: 'rQQQrUdN1cLdNmxH4dHfKgmX5P4kf3ZrM',
     destination_amount: xrpToDrops(10),
-    // send_max?: Amount,
-    // paths?: Path[]
   })
 }
 
